7a93567ec8312f9eb75b411cd1b03e400f9ea98a
NN_98 <44117915+NN198@users.noreply.github.com>
2024-12-17 19:35:40 +0000

    test deploment

--- fintech-app/src/app/page.tsx
+++ fintech-app/src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Profile(){
     return(
         <>
         <Header />
-      
         </>
     )
 }
--- fintech-app/src/app/pages/posts/[id]/page.tsx
+++ fintech-app/src/app/pages/posts/[id]/page.tsx
@@ -14,42 +14,44 @@ type Post = {
 //     return response.json()
 // }
 
-export default async function PostID({params}: any) {
-    // const {post} = await getPostsByID(params.id)
-    const [post, setPost] = useState<Post | null>(null)
+// export default  function PostID({params}: any) {
+//     // const {post} = await getPostsByID(params.id)
+//     const [post, setPost] = useState<Post | null>(null)
 
-    const getPostsById = async () => {
-        try {
-            const response = await fetch(
-                `http://localhost:3000/pages/api/posts/${params.id}`,
-                {method: 'GET'}
-            )
-            if (response) {
-                // const data = await response.json()
-                //  console.log(data)
-                const {post} = await response.json()
-                if(post) setPost(post)
-            }
+//     const getPostsById = async () => {
+//         try {
+//             const response = await fetch(
+//                 `http://localhost:3000/pages/api/posts/${params.id}`,
+//                 {method: 'GET'}
+//             )
+//             if (response) {
+//                 // const data = await response.json()
+//                 //  console.log(data)
+//                 const {post} = await response.json()
+//                 if(post) setPost(post)
+//             }
 
-        }
-        catch(error){
-            console.log(error);
+//         }
+//         catch(error){
+//             console.log(error);
             
-        }
+//         }
         
-    }
+//     }
 
-    useEffect(() =>{
-        getPostsById()
-    }, [])
+//     useEffect(() =>{
+//         getPostsById()
+//     }, [])
     
-    return(
-    <main>
-        {post && <h1>{post.title}</h1>}
-        {post && <p>{post.description}</p>}
-        </main>
-    );
-}
+//     return(
+//     <main>
+//         {post && <h1>{post.title}</h1>}
+//         {post && <p>{post.description}</p>}
+//         </main>
+//     );
+// }
+
+
 
 //now how to use above server components for client side
 
